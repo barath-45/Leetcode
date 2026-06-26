2c3594fb785101f2c80e30823577bdd43092a150
barath-45 <bharathmoorthy05@gmail.com>
2026-06-26 21:21:02 +0530

    Time: 3 ms (92.54%), Space: 46.8 MB (5.03%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char []arr=s.toCharArray();
+        char[]arr1=t.toCharArray();
+        Arrays.sort(arr);
+        Arrays.sort(arr1);
+        return Arrays.equals(arr,arr1);
+    }
+}

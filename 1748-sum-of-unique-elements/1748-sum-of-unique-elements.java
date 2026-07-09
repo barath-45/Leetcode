2d64aa452cf11c1534cffaa9280cbd7382706e28
barath-45 <bharathmoorthy05@gmail.com>
2026-07-09 15:38:05 +0530

    Time: 2 ms (60.23%), Space: 43.2 MB (19.17%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+       HashMap<Integer,Integer> map=new HashMap<>();
+       for(int n1:nums){
+        map.put(n1,map.getOrDefault(n1,0)+1);
+       }
+       int sum=0;
+       for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+        if(entry.getValue()==1){
+            sum+=entry.getKey();
+            
+        }
+       }
+       return sum;
+    }
+}

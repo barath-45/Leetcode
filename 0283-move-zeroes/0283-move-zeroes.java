f75532430bc40d365eccb188e0e1c2cc5135930f
barath-45 <bharathmoorthy05@gmail.com>
2026-07-09 11:31:32 +0530

    Time: 1 ms (99.84%), Space: 48 MB (20.21%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0;
+        for(int x:nums){
+            if(x!=0){
+                nums[i]=x;
+                i++;
+            }
+        }
+        while(i<nums.length){
+            nums[i]=0;
+            i++;
+        }
+    }
+}

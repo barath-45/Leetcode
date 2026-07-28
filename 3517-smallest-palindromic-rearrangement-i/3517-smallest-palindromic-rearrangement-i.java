743ea0d99b3e2f1a07223e2a5772aef09610e346
barath-45 <bharathmoorthy05@gmail.com>
2026-07-28 20:19:17 +0530

    Time: 33 ms (43.78%), Space: 48.4 MB (10.04%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -1,27 +1,27 @@
 class Solution {
     public String smallestPalindrome(String s) {
-       int[]freq=new int[26];
-       for(char ch:s.toCharArray()){
-        freq[ch-'a']++;
-       }
-       char middle=' ';
-       for(int i=0;i<26;i++){
-        if(freq[i]%2==1){
-            middle=(char)(i+'a');
+        int freq[]=new int[26];
+        for(char ch:s.toCharArray()){
+            freq[ch-'a']++;
         }
-       }
-       StringBuilder sb=new StringBuilder();
-       for(int i=0;i<26;i++){
-        int pairs=freq[i]/2;
-        for(int j=0;j<pairs;j++){
-            sb.append((char)(i+'a'));
+        char middle='a';
+        for(int i=0;i<26;i++){
+            if(freq[i]%2==1){
+                middle=(char)(i+'a');
+            }
         }
-       }
-       String half=sb.toString();
-       String half2=sb.reverse().toString();
-       if(s.length()%2==1){
-        return half+middle+half2;
-       }
-       return half+half2;
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<26;i++){
+            int par=freq[i]/2;
+            for(int j=0;j<par;j++){
+                sb.append((char)(i+'a'));
+            }
+        }
+        String hf=sb.toString();
+        String hfr=sb.reverse().toString();
+        if(s.length()%2==1){
+            return hf+middle+hfr;
+        }
+        return hf+hfr;
     }
 }

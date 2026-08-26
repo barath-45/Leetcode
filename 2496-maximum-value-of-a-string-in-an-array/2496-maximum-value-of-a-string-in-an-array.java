6bb9bb1aa0b05cbc55761bf7738f361a2e020263
barath-45 <bharathmoorthy05@gmail.com>
2026-08-26 23:59:01 +0530

    Time: 1 ms (92.82%), Space: 43.1 MB (40.78%) - LeetHub

--- 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.java
+++ 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.java
@@ -1,24 +1,25 @@
 class Solution {
     public int maximumValue(String[] strs) {
         int max=0;
-       for(String x:strs){
-            boolean dig=true;
-             for(char ch:x.toCharArray()){
+        for(String x:strs){
+            boolean pg=true;
+            for(char ch:x.toCharArray()){
                 if(ch < '0' || ch > '9'){
-                    dig=false;
+                    pg=false;
                     break;
                 }
-             }
-             int val;
-             if(dig){
-                val=Integer.parseInt(x);
-             }else{
-                val=x.length();
-             }
-             if(val > max){
-                max=val;
-             }
-       }
-       return max; 
+            }
+            int value;
+            if(pg){
+                value=Integer.parseInt(x);
+            }
+            else{
+                value=x.length();
+            }
+            if(value > max){
+                max = value;
+            }
+        }
+        return max;
     }
 }
